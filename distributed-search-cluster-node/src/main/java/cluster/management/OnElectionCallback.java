33e13e48de81e8867826ecc9af1f8889a400971e
tylyuu <lutianyue56@gmail.com>
2023-12-19 17:19:09 -0500

    added backend cluster

---
+++ distributed-search-cluster-node/src/main/java/cluster/management/OnElectionCallback.java
@@ -0,0 +1,10 @@
+
+package cluster.management;
+
+public interface OnElectionCallback {
+
+    void onElectedToBeLeader();
+
+    void onWorker();
+
+}
---
+++ distributed-search-cluster-node/src/main/java/networking/OnRequestCallback.java
@@ -0,0 +1,8 @@
+
+package networking;
+
+public interface OnRequestCallback {
+    byte[] handleRequest(byte[] requestPayload);
+
+    String getEndpoint();
+}
